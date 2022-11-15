1a0ed2656c679e1df223607920ba7e2ecbeb7349
Matt <mattnazario@gmail.com>
2022-11-15 09:11:45 -0600

    Add premade arrays of random monsters,item bases, and affixes

---
+++ README.md
@@ -0,0 +1,4 @@
+#ITEM GENERATOR 
+=
+Personal project, testing out javascript to make a random item generator!
+Pulls a random item with a random affix from a random monster!
--- itemgen.js
+++ itemgen.js
@@ -1,2 +1,9 @@
-console.log("HELLO");
-console.log("IS THIS THING ON?");
+//Initializing Array with premade values.
+let monsterArr = ['skeleton','bat','orc','goblin','dragon','zombie'];
+let itemArr = ['sword','plate chest','belt','bow','mace','axe','leggings','robe','gloves','helmet','wizard hat','dagger'];
+let affixArr = ['Intellect','Strength','Dexterity','Power','Dodging','Fire','Frost','Lightning','Arcane','Shadow','Poison','Blocking','Resistance'];
+
+//Generate random index for each array.
+let monsterIndex = Math.floor(Math.random() * monsterArr.length);
+let itemIndex = Math.floor(Math.random() * itemArr.length);
+let affixIndex = Math.floor(Math.random() *  affixArr.length);
